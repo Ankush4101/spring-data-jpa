b9dece756b6495b63c65237ef38b6484d7377094
mama-samba-braima <djalonelson@gmail.com>
2025-02-05 14:47:56 +0000

    pagination sorting and limit

--- src/main/java/com/amigoscode/App.java
+++ src/main/java/com/amigoscode/App.java
@@ -1,9 +1,14 @@
 package com.amigoscode;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 @SpringBootApplication
 public class App {
@@ -15,38 +20,50 @@ public class App {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
         return args -> {
-            Student student = new Student(
-                    "Foo",
-                    "Bar",
-                    18,
-                    "foobar@amigoscode.com"
-            );
-            studentRepository.save(student);
+            generateRandomStudents(studentRepository);
             System.out.println(studentRepository.count());
             System.out.println(studentRepository.findAll());
-            System.out.println(studentRepository.findById(1L));
-            System.out.println(studentRepository.existsById(1L));
-            System.out.println(studentRepository.existsById(2L));
-            System.out.println(studentRepository.findStudentByEmail(
-                    "foobar@amigoscode.com"
-            ));
-            System.out.println(
-                    studentRepository
-                            .findStudentsByFirstNameEqualsAndAgeGreaterThanEqual(
-                                    "Foo", 18
-                            ));
-            // studentRepository.deleteById(1L);
-            studentRepository.deleteStudentByEmail("foobar@amigoscode.com");
-            System.out.println(studentRepository.count());
-            System.out.println(studentRepository.findStudentByEmail(
-                    "foobar@amigoscode.com"
-            ));
-            System.out.println(
-                    studentRepository
-                            .findStudentsByFirstNameEqualsAndAgeGreaterThanEqualNative(
-                                    "foo", 23
-                            ));
         };
+    }
+
+    private static void paging(StudentRepository studentRepository) {
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Student> page = studentRepository.findAll(pageable);
+        System.out.println(page);
+        System.out.println(page.getTotalPages());
+        System.out.println(page.getTotalElements());
+        System.out.println(page.getSize());
+        page.get().forEach(System.out::println);
+        pageable = PageRequest.of(11, 1000);
+        page = studentRepository.findAll(pageable);
+        System.out.println("--------");
+        page.get().forEach(System.out::println);
+    }
+
+    private static void sort(StudentRepository studentRepository) {
+        Sort sort = Sort.by("firstName").descending()
+                .and(Sort.by("age").descending());
+        studentRepository.findAll(sort).forEach(s -> {
+            System.out.println(s.getFirstName() + " " + s.getAge());
+        });
+    }
+
+    private static void generateRandomStudents(StudentRepository studentRepository) {
+        Faker faker = new Faker();
+        for (int i = 0; i < 100; i++) {
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String email = """
+                    %s.%s@amigoscode.edu
+                    """.formatted(firstName, lastName);
+            Student student = new Student(
+                    firstName,
+                    lastName,
+                    faker.number().numberBetween(17, 55),
+                    email
+            );
+            studentRepository.save(student);
+        }
     }
 
 }
---
+++ src/main/java/com/amigoscode/QueryLimitInspector.java
@@ -0,0 +1,14 @@
+package com.amigoscode;
+
+import org.hibernate.resource.jdbc.spi.StatementInspector;
+
+public class QueryLimitInspector implements StatementInspector {
+    @Override
+    public String inspect(String sql) {
+        if(sql.toLowerCase().startsWith("select") &&
+            !sql.toLowerCase().contains("limit")) {
+            return sql + " LIMIT 1000";
+        }
+        return sql;
+    }
+}
