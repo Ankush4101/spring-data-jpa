4bea7bfacabbd190b64c4431337febc57f60efac
mama-samba-braima <djalonelson@gmail.com>
2025-02-05 11:07:20 +0000

    repositories

--- src/main/java/com/amigoscode/App.java
+++ src/main/java/com/amigoscode/App.java
@@ -1,13 +1,35 @@
 package com.amigoscode;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student student = new Student(
+                    "Foo",
+                    "Bar",
+                    18,
+                    "foobar@amigoscode.com"
+            );
+            studentRepository.save(student);
+            System.out.println(studentRepository.count());
+            System.out.println(studentRepository.findAll());
+            System.out.println(studentRepository.findById(1L));
+            System.out.println(studentRepository.existsById(1L));
+            System.out.println(studentRepository.existsById(2L));
+            studentRepository.deleteById(1L);
+            System.out.println(studentRepository.count());
+        };
     }
 
 }
